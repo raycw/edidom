b61792b1a8eabf60a6e69e669c9a587fc3f58abf
raymond.chin.wai <raymond.chin.wai@78e8ac39-4764-8c78-c7d0-797e43201622>
2010-10-14 15:58:38 +0000

    git-svn-id: http://edi-common.googlecode.com/svn/trunk@14 78e8ac39-4764-8c78-c7d0-797e43201622

---
+++ src/main/java/com/cargosmart/b2b/edi/output/X12Outputter.java
@@ -0,0 +1,19 @@
+package com.cargosmart.b2b.edi.output;
+
+import com.cargosmart.b2b.edi.common.Document;
+
+/**
+ * Output X12 Document to a string or writer. The {@link #outputString(Document) outputString} 
+ * method to print out X12 format string.
+ * 
+ * 
+ * @author Raymond
+ *
+ */
+public class X12Outputter {
+
+	public String outputString(Document document) {
+		
+		return null;
+	}
+}
